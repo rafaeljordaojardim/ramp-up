ccb6b16ad64cb48c256c4f41a86dac28b3b55c8b
rjardem <rjardem@vmware.com>
2019-10-18 15:46:41 -0300

    putting a trying logic to try 3 times

--- src/repository/weather/index.ts
+++ src/repository/weather/index.ts
@@ -1,18 +1,40 @@
 import forecast from '../../clients/wheather/darksky/forecast';
 import geocode from '../../clients/wheather/mapbox/geocode';
 import Forecast from '../../models/interfaces/forecast';
+import ErrorHandling from '../../errorHandling/error';
+import { STATUS_CODES } from 'http';
 
 class RepositoryWeather {
     constructor() {
         
     }
     public async getWeather(address:string):Promise<Forecast> {
-        const geo = await geocode(address);
-        if(!geo){
-            throw new Error('Geocode not set')
+        let geo;
+        let count = 0;
+        while(!geo || count<=3){
+            geo = await geocode(address);
+            console.log('try ' + count);
+            count++;
         }
-        const fore = await forecast(geo.latitude, geo.longitude);
+        if(!geo){
+            throw new ErrorHandling(STATUS_CODES[404], 404);
+        }
+        count = 0;
+        let fore;
+        while(!fore || count <=3){
+            fore = await forecast(geo.latitude, geo.longitude);
+            console.log('try ' + count);
+            count++;
+            
+        }
+        if(!fore){
+            throw new ErrorHandling(STATUS_CODES[404], 404);
+        }
         return {...fore, location:geo.location};
+    }
+
+    private async getToTryGetTheAwnser(){
+
     }
 }
 export default RepositoryWeather;
